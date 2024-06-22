eb033754d2540b2e2e9b030780139641ea438152
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-06-21 21:38:29 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3,7 +3,7 @@ class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.angle = Math.PI / 2; // Start at 90 degrees (pointing upwards)
+        this.angle = 0; // Start at 0 degrees (pointing right)
         this.speed = 0;
         this.maxSpeed = 5;
         this.acceleration = 0.1;
@@ -13,8 +13,8 @@ class Player {
     update() {
         // Update position based on current speed and angle
         this.x += Math.cos(this.angle) * this.speed;
-        this.y += Math.sin(this.angle) * this.speed;
-
+        this.y -= Math.sin(this.angle) * this.speed; // Note the minus sign here
+        
         // Apply drag to gradually slow down
         this.speed *= 0.99;
 
@@ -28,7 +28,7 @@ class Player {
     draw(ctx) {
         ctx.save();
         ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(this.angle);
+        ctx.rotate(-this.angle); // Note the negative angle here
         drawShape(ctx, SHAPES.PLAYER, 0, 0, SIZES.PLAYER, COLORS.PLAYER);
         ctx.restore();
     }
@@ -44,12 +44,12 @@ class Player {
     }
 
     rotateLeft() {
-        this.angle -= this.rotationSpeed;
+        this.angle += this.rotationSpeed;
         console.log(`Rotating left. New angle: ${(this.angle * 180 / Math.PI).toFixed(2)}°`);
     }
 
     rotateRight() {
-        this.angle += this.rotationSpeed;
+        this.angle -= this.rotationSpeed;
         console.log(`Rotating right. New angle: ${(this.angle * 180 / Math.PI).toFixed(2)}°`);
     }
 
